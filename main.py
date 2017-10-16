40d01908035a1dc7ace7a909d1256c8abb45f6a4
kou_fukase <kou.ddsat@gmail.com>
2017-10-16 16:49:20 +0900

    [update]main union関数を使用可能に

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import argparse
 """
 def union(args):
 	categories = load_categories()
-	pic_category = set(args.category)
+	pic_category = args.categories
+	result = categories.search_by_stuff(pic_category)
+	if result:
+		print(result)
+	else:
+		print('この組み合わせでは合体できません')
 
 
 """
@@ -21,7 +26,7 @@ def main():
 	subparser = parser.add_subparsers()
 
 	union_parser = subparser.add_parser('union', help='2身合体')
-	union_parser.add_argument('category', nargs=2, help='種族')
+	union_parser.add_argument('categories', nargs=2, help='種族')
 	union_parser.set_defaults(func=union)
 
 	possible_parser = subparser.add_parser('pos', help='合体可能な種族を検索します')
