71406296a26598c134b7776c97e566ddce87d26b
kou_fukase <kou.ddsat@gmail.com>
2017-10-16 16:49:51 +0900

    [fix]インデントをスペースに変換

--- category.py
+++ category.py
@@ -9,102 +9,102 @@ name: 種族名
 stuffs: 合体に必要な素材
 """
 class Category:
-	def __init__(self, name, stuffs):
-		self.name = name
-		self.stuffs = stuffs
+    def __init__(self, name, stuffs):
+        self.name = name
+        self.stuffs = stuffs
 
-	"""
-	指定素材と一致する場合、組み合わせを返す
-	"""
-	def match_stuff(self, target):
-		match_stuffs = []
-		if isinstance(target, str):
-			target = [target]
-		for stuff in self.stuffs:
-			if set(target) <= set(stuff):
-				match_stuffs.append(stuff)
-		return match_stuffs
+    """
+    指定素材と一致する場合、組み合わせを返す
+    """
+    def match_stuff(self, target):
+        match_stuffs = []
+        if isinstance(target, str):
+            target = [target]
+        for stuff in self.stuffs:
+            if set(target) <= set(stuff):
+                match_stuffs.append(stuff)
+        return match_stuffs
 
 
 """
 種族情報操作クラス
 """
 class Categories:
-	def __init__(self, categories):
-		self.categories = categories
+    def __init__(self, categories):
+        self.categories = categories
 
-	"""
-	種族名からオブジェクトを返す
-	"""
-	def get_category(self, name):
-		for category in self.categories:
-			if category.name == name:
-				return category
-		return False
+    """
+    種族名からオブジェクトを返す
+    """
+    def get_category(self, name):
+        for category in self.categories:
+            if category.name == name:
+                return category
+        return False
 
-	"""
-	指定素材にマッチする種族と素材リストを返す
+    """
+    指定素材にマッチする種族と素材リストを返す
 
-	>>> ['a']
-	{category1: [{'a', 'b'}, {'a', 'd'}], category2: [{'a', 'y'}]}
-	>>> ['a', 'b']
-	{category1: [{'a', 'b'}]}
-	"""
-	def search_by_stuff(self, stuffs):
-		match_categories = {}
-		for category in self.categories:
-			if category.match_stuff(stuffs):
-				match_categories[category.name] = category.match_stuff(stuffs)
-		return match_categories
+    >>> ['a']
+    {category1: [{'a', 'b'}, {'a', 'd'}], category2: [{'a', 'y'}]}
+    >>> ['a', 'b']
+    {category1: [{'a', 'b'}]}
+    """
+    def search_by_stuff(self, stuffs):
+        match_categories = {}
+        for category in self.categories:
+            if category.match_stuff(stuffs):
+                match_categories[category.name] = category.match_stuff(stuffs)
+        return match_categories
 
-	"""
-	種族情報更新
-	"""
-	def update(self):
-		categories_list = []
-		# カテゴリーオブジェクトをjsonに変換できる形式に変換
-		for category in self.categories:
-			category_dict = {}
-			category_dict['name'] = category.name
-			category_dict['stuffs'] = []
-			for stuff in category.stuffs:
-				category_dict['stuffs'].append(stuff)
-			categories_list.append(category_dict)
-		with open('data/category.json', mode='w', encoding='utf-8') as f:
-			json.dump(categories_list, f, ensure_ascii=False, indent="\t")
+    """
+    種族情報更新
+    """
+    def update(self):
+        categories_list = []
+        # カテゴリーオブジェクトをjsonに変換できる形式に変換
+        for category in self.categories:
+            category_dict = {}
+            category_dict['name'] = category.name
+            category_dict['stuffs'] = []
+            for stuff in category.stuffs:
+                category_dict['stuffs'].append(stuff)
+            categories_list.append(category_dict)
+        with open('data/category.json', mode='w', encoding='utf-8') as f:
+            json.dump(categories_list, f, ensure_ascii=False, indent="\t")
 
-	"""
-	合体素材情報登録
-	"""
-	def add_stuffs(name, stuffs):
-		categories = load_categories()
-		target_category = categories.get_category(name)
-		if target_category:
-			target_category.stuffs.append(stuffs)
-			categories.update()
+    """
+    合体素材情報登録
+    """
+    def add_stuffs(name, stuffs):
+        categories = load_categories()
+        target_category = categories.get_category(name)
+        if target_category:
+            target_category.stuffs.append(stuffs)
+            categories.update()
 
 
 """
 種族情報を読み込み
 """
 def load_categories():
-	category_list = []
-	with open('data/category.json', encoding='utf-8') as f:
-		# Json形式を読み込み
-		json_data = json.load(f)
-		for line in json_data:
-			# 種族名
-			name = line['name']
-			# 必要素材
-			stuffs = []
-			if line.setdefault('stuffs'):
-				stuffs = line['stuffs']
+    category_list = []
+    with open('data/category.json', encoding='utf-8') as f:
+        # Json形式を読み込み
+        json_data = json.load(f)
+        for line in json_data:
+            # 種族名
+            name = line['name']
+            # 必要素材
+            stuffs = []
+            if line.setdefault('stuffs'):
+                stuffs = line['stuffs']
 
-			category = Category(name, stuffs)
-			category_list.append(category)
-	categories = Categories(category_list)
-	return categories
+            category = Category(name, stuffs)
+            category_list.append(category)
+    categories = Categories(category_list)
+    return categories
 
 
 if __name__ == '__main__':
-	pass
+    pass
--- main.py
+++ main.py
@@ -6,35 +6,35 @@ import argparse
 悪魔合体
 """
 def union(args):
-	categories = load_categories()
-	pic_category = args.categories
-	result = categories.search_by_stuff(pic_category)
-	if result:
-		print(result)
-	else:
-		print('この組み合わせでは合体できません')
+    categories = load_categories()
+    pic_category = args.categories
+    result = categories.search_by_stuff(pic_category)
+    if result:
+        print(result)
+    else:
+        print('この組み合わせでは合体できません')
 
 
 """
 """
 def pos(args):
-	pass
+    pass
 
 
 def main():
-	parser = argparse.ArgumentParser(description='合体検索')
-	subparser = parser.add_subparsers()
+    parser = argparse.ArgumentParser(description='合体検索')
+    subparser = parser.add_subparsers()
 
-	union_parser = subparser.add_parser('union', help='2身合体')
-	union_parser.add_argument('categories', nargs=2, help='種族')
-	union_parser.set_defaults(func=union)
+    union_parser = subparser.add_parser('union', help='2身合体')
+    union_parser.add_argument('categories', nargs=2, help='種族')
+    union_parser.set_defaults(func=union)
 
-	possible_parser = subparser.add_parser('pos', help='合体可能な種族を検索します')
-	possible_parser.add_argument('category', type=str, help='対象の種族')
-	possible_parser.set_defaults(func=pos)
+    possible_parser = subparser.add_parser('pos', help='合体可能な種族を検索します')
+    possible_parser.add_argument('category', type=str, help='対象の種族')
+    possible_parser.set_defaults(func=pos)
 
-	args = parser.parse_args()
-	args.func(args)
+    args = parser.parse_args()
+    args.func(args)
 
 if __name__ == '__main__':
-	main()
+    main()
