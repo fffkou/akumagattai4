08eb4ae8b2277eddd394ee10c5d332f74253598c
kou_fukase <kou.ddsat@gmail.com>
2017-10-19 17:59:28 +0900

    [update]fusion same_fusion関数を外部依存に変更
    spiritモジュールから呼び出して実行

--- fusion.py
+++ fusion.py
@@ -9,20 +9,11 @@ import json
 def same_fusion(name):
     categories = load_categories()
     target = categories.get_category(name)
-    spirit_dict = {
-        "アーシーズ": set(['妖樹', '魔獣', '地霊', '妖虫', '夜魔']),
-        "エアロス": set(['天使', '妖魔', '凶鳥', '妖精', '悪霊']),
-        "アクアンズ": set(['妖鳥', '妖獣', '妖鬼', '鬼女', '幽鬼']),
-        "フレイミーズ": set(['天女', '龍王', '邪鬼', '堕天使', '邪龍']),
-        "ノーム": set(['神樹', '死神', '地母神', '鬼神']),
-        "シルフ": set(['霊鳥', '聖獣', '破壊神', '龍王']),
-        "ウンディーネ": set(['女神', '邪神', '神獣', '幻魔']),
-        "サラマンダー": set(['大天使', '魔神', '魔王', '国津神'])
-    }
+    spirits = load_spirits()
     if target:
-        for key, value in spirit_dict.items():
-            if name in value:
-                return key
+        result = spirits.search_by_stuff(name)
+        if result:
+            return result.name
     return False
 
 
