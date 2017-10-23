e033deb310c9b36fd88e2b7a4603009ca6c9b764
kou_fukase <kou.ddsat@gmail.com>
2017-10-23 18:13:26 +0900

    [update]main fusionコマンド追加
    悪魔名から結果を求めるコマンド

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from category import Category, Categories, load_categories
-from fusion import same_fusion
+from fusion import same_fusion, normal_fusion
 from devil import load_devils, Devils, Devil
 import argparse
 
@@ -7,15 +7,10 @@ import argparse
 """
 悪魔合体
 """
-def union(args):
-    categories = load_categories()
-    pic_category = args.categories
-    # 同種の場合精霊合体
-    if len(pic_category) == 1:
-        result = same_fusion(pic_category[0])
-    # 通常合体
-    else:
-        result = categories.search_by_stuff(pic_category)        
+
+
+def fusion(args):
+    result = normal_fusion(args.devil_name1, args.devil_name2)
     if result:
         print(result)
     else:
@@ -65,9 +60,10 @@ def main():
     parser = argparse.ArgumentParser(description='合体検索')
     subparser = parser.add_subparsers()
 
-    union_parser = subparser.add_parser('union', help='2身合体')
-    union_parser.add_argument('categories', nargs='+', help='種族')
-    union_parser.set_defaults(func=union)
+    fusion_parser = subparser.add_parser('fusion', help='2身合体')
+    fusion_parser.add_argument('devil_name1', type=str, help='悪魔1')
+    fusion_parser.add_argument('devil_name2', type=str, help='悪魔2')
+    fusion_parser.set_defaults(func=fusion)
 
     need_parser = subparser.add_parser('need', help='合体に必要な素材を検索します')
     need_parser.add_argument('target', type=str, help='作成したい種族')
